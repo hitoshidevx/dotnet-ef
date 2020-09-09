07aba28b36a2565f0348dcdeb91eae95c13ab0b2
hitoshi <g.hitoshi.yokogawa@gmail.com>
2020-09-08 23:07:10 -0300

    Contexts atualizados.

---
+++ dotnet-ef/dotnet-ef/Context/PedidoContext.cs
@@ -0,0 +1,23 @@
+﻿using dotnet_ef.Domains;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_ef.Context
+{
+    public class PedidoContext : DbContext
+    {
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<PedidoItem> PedidosItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseSqlServer(@"Data source=.\Sqlexpress;Initial Catalog=Db_Senai_Pedidos_Dev;user id=sa; password=sa@132");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
